33f95018d9728c5607ff947d4137d4fb9ec066a7
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-11-03 13:48:16 +0000

    completed

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+import 'react-native-gesture-handler';
+import { AppRegistry } from 'react-native';
+import LoginPage from './src/LoginPage';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => LoginPage);
